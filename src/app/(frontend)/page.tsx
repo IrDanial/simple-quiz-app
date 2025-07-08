e1f4ac35b7d60eb52d8f36f5e96696ac70d27dcb
Irfani <369indonesia@gmail.com>
2025-07-08 17:02:32 +0700

    feat: regist
    feat: regist

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -10,5 +10,27 @@ import '../globals.css'
 import RegistrationForm from '@/components/registration-form'
 
 export default async function HomePage() {
+  // Hanya ada di server
+  const payload = await getPayload({ config })
+
+  const res = await payload.find({
+    collection: 'users',
+  })
+
+  // return res.docs.map((user) => user.email)
+
   return <RegistrationForm></RegistrationForm>
+  // return <SignUpForm></SignUpForm>
 }
+
+/* 
+TODO PR Server Action
+1. Buat server action untuk nerima email, password, dan fullName lalu buat user melalui payload object (payload.create({collection: "users"}))
+2. Export server action itu lalu import ke registration form
+3. Di registration form, buat fungsi async yang manggil server action itu dan kirimkan input dari user sebagai argumen
+4. Coba cek Payload Dashboard (/admin) jika usersnya terbuat atau tidak
+
+// Sumber belajar:
+- Next.js Server Action
+- Payload CMS auth collections
+*/
